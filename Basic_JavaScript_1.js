d9f07cc000beb84adc802498626232ca8616fa5a
vizorius <vmvidmantas@gmail.com>
2025-05-22 22:38:37 +0100

    script added to html

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,5 @@
-alert("Alert, you under learning basics!")
+function My_First_Function() {
+  var text = "Kiss me, I'm Irish!";
+  var result = text.fontcolor("green");
+  document.getElementById("Irish").innerHTML = result;
+}
